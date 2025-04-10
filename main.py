6c2795836ffadbe63f2aba9c20697d32a9db26ba
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-10 11:02:33 +0300

    QTstart

--- main.py
+++ main.py
@@ -1,66 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QListWidget, QVBoxLayout, QListWidgetItem, QLabel, QComboBox, \
-    QLineEdit, QPushButton
-import pymysql.cursors
+from PyQt6.QtWidgets import QApplication, \
+    QWidget, QGridLayout, QLabel, QPushButton, QLineEdit
+from qt_material import apply_stylesheet
 
-#https://ctxt.io/2/AAB4kwukFg
+
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print("Подключение к БД...")
-        self.cnx = pymysql.connect(host="localhost",
-                         user="pk32",
-                         password="1234",
-                         database='water_shop')
-        print("Подключение успешно.")
-        # set the window title
-        self.setWindowTitle('Магазин Ромашка')
-        self.tovari_view = QListWidget()
-        pos_label = QLabel("Позиция:")
-        self.pos_entry = QComboBox()
-        self.get_table_fields()
-        #SHOW COLUMNS FROM my_table;
-        znak_label = QLabel("Знак:")
-        self.znak_entry = QComboBox()
-        self.znak_entry.addItems([">","<","="])
-        chiselka_label = QLabel("чиселка/буковка:")
-        self.chiselka_entry = QLineEdit()
-        self.btn = QPushButton("ТЫК!")
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.tovari_view)
-        layout.addWidget(pos_label)
-        layout.addWidget(self.pos_entry)
-        layout.addWidget(znak_label)
-        layout.addWidget(self.znak_entry)
-        layout.addWidget(chiselka_label)
-        layout.addWidget(self.chiselka_entry)
-        layout.addWidget(self.btn)
+        self.setGeometry(100, 100, 500, 300)
+        self.setWindowTitle('Hello World')
+        apply_stylesheet(app, theme='light_blue.xml')
+        layout = QGridLayout()
         self.setLayout(layout)
+        lbl = QLabel("пуп")
+        layout.addWidget(lbl, 0, 0)
+        btn = QPushButton("ПУП!!!")
+        layout.addWidget(btn, 1, 1)
+        edit1 = QLineEdit()
+        layout.addWidget(edit1,0,1)
+        edit2 = QLineEdit()
+        layout.addWidget(edit2,0,2)
         # show the window
-        self.select_sklad()
         self.show()
-
-    def select_sklad(self):
-        cursor = self.cnx.cursor()
-        cursor.execute("SELECT * from sklad WHERE quantity < 5")
-        ans = cursor.fetchall()
-        for item in ans:
-            tmp = f"{item[1]}, {item[2]}, {item[3]}, {item[4]}"
-            qtmp = QListWidgetItem(tmp)
-            self.tovari_view.addItem(qtmp)
-
-    def get_table_fields(self):
-        cursor = self.cnx.cursor()
-        cursor.execute("SHOW COLUMNS FROM sklad")
-        ans = cursor.fetchall()
-        print(ans)
-        for item in ans:
-            print(item[0])
-            self.pos_entry.addItem(item[0])
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # create the main window
     window = MainWindow()
+    # start the event loop
     sys.exit(app.exec())
