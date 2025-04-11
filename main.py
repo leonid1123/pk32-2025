eb1f6b115a9af13e6e8443c47889da11e30cae37
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-11 11:48:25 +0300

    студенческий отдел кадров

--- main.py
+++ main.py
@@ -1,34 +1,102 @@
 import sys
 from PyQt6.QtWidgets import QApplication, \
-    QWidget, QGridLayout, QLabel, QPushButton, QLineEdit
+    QWidget, QGridLayout, QLabel, QPushButton, QLineEdit, QListWidget, QMessageBox
 from qt_material import apply_stylesheet
+import pymysql.cursors
+
+
+class Student:
+    def __init__(self,_name,_fam,_otch,_dr,_tel,_id):
+        self.name = _name
+        self.fam = _fam
+        self.otch = _otch
+        self.dr = _dr
+        self.tel = _tel
+        self.id = _id
 
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setGeometry(100, 100, 500, 300)
-        self.setWindowTitle('Hello World')
+        self.students = []
+        self.setGeometry(100, 100, 500, 600)
+        self.setWindowTitle('Студенческий отдел кадров')
         apply_stylesheet(app, theme='light_blue.xml')
         layout = QGridLayout()
         self.setLayout(layout)
-        self.lbl = QLabel("пуп")
-        layout.addWidget(self.lbl, 0, 0)
-        btn = QPushButton("ПУП!!!", )
-        btn.clicked.connect(self.my_slot)
-        layout.addWidget(btn, 1, 1)
-        self.edit1 = QLineEdit()
-        layout.addWidget(self.edit1, 0, 1)
-        self.edit2 = QLineEdit()
-        layout.addWidget(self.edit2, 0, 2)
+        self.student_view = QListWidget()
+        self.fam_entry = QLineEdit()
+        self.name_entry = QLineEdit()
+        self.otchestvo_entry = QLineEdit()
+        self.phone_entry = QLineEdit()
+        self.data_rozhdeniya_entry = QLineEdit()
+        self.add_button = QPushButton("Добавить")
+        self.add_button.clicked.connect(self.add_student)
+        self.edit_button = QPushButton("Изменить")
+        self.edit_button.clicked.connect(self.change_student)
+
+        layout.addWidget(self.student_view, 0, 0, 4, 0)
+        layout.addWidget(self.fam_entry, 1, 1)
+        layout.addWidget(self.name_entry, 1, 3)
+        layout.addWidget(self.otchestvo_entry, 2, 1)
+        layout.addWidget(self.data_rozhdeniya_entry, 2, 3)
+        layout.addWidget(self.phone_entry, 3, 1)
+        layout.addWidget(self.add_button, 3, 2, 1, 2)
+        layout.addWidget(self.edit_button, 4, 0, 1, 2)
+
+        layout.addWidget(QLabel("Фамилия"), 1, 0)
+        layout.addWidget(QLabel("Имя"), 1, 2)
+        layout.addWidget(QLabel("Отчество"), 2, 0)
+        layout.addWidget(QLabel("Дата рождения"), 2, 2)
+        layout.addWidget(QLabel("Телефон"), 3, 0)
+
+        self.cnx = pymysql.connect(
+            host="localhost",
+            user="pk32",
+            password="1234",
+            database="stud_otdel")
+        self.cur = self.cnx.cursor()
+        self.get_all_students()
         # show the window
         self.show()
 
-    def my_slot(self):
-        print("ОЙ!!!")
-        str1 = self.edit1.text()
-        str2 = self.edit2.text()
-        self.lbl.setText(str1 + str2)
+    def get_all_students(self):
+        sql = "SELECT * FROM students"
+        self.cur.execute(sql)
+        ans = self.cur.fetchall()
+        for item in ans:
+            st = Student(item[1],item[2],item[3],item[4],item[5],item[0])
+            self.students.append(st)
+            self.student_view.addItem(
+                f"{item[1]}, {item[2]}, {item[3]}, {item[4]}")
+
+    def add_student(self):
+        """добавление студентов в базу"""
+        name = self.name_entry.text()
+        fam = self.fam_entry.text()
+        otch = self.otchestvo_entry.text()
+        dr = self.data_rozhdeniya_entry.text()
+        phone = self.phone_entry.text()
+        self.student_view.clear()
+        if name and fam and otch and dr and phone:
+            sql = """INSERT INTO 
+            students(imya, familiya, 
+            otchestvo, data_rozhdeniya, telephone)
+             VALUES(%s,%s,%s,%s,%s)"""
+            params = (name, fam, otch, dr, phone)
+            self.cur.execute(sql, params)
+            self.cnx.commit()
+            self.get_all_students()
+        else:
+            QMessageBox.warning(self, "Внимание!", "Поля должны быть заполнены!")
+
+    def change_student(self):
+        """метод для изменения информации о студенте"""
+        x = self.student_view.currentRow()
+        id = self.students[x].id
+        #перенести в другой метод
+        self.name_entry.clear()
+        self.name_entry.insert(self.students[x].name)
 
 
 if __name__ == '__main__':
