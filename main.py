8a5e26d594a438490886582b19653e9deb8cf390
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-25 12:11:33 +0300

    локальный чат. начато окно для логина. Нужен статический класс для бд

--- main.py
+++ main.py
@@ -1,121 +1,90 @@
 import sys
-from PyQt6.QtWidgets import QApplication, \
-    QWidget, QGridLayout, QLabel, QPushButton, QLineEdit, QListWidget, QMessageBox
-from qt_material import apply_stylesheet
+from PyQt6.QtWidgets import QApplication, QWidget, \
+    QGridLayout, QListWidget, QLineEdit, \
+    QPushButton, QMessageBox, QVBoxLayout, QLabel
+from PyQt6.QtCore import QTimer
 import pymysql.cursors
 
 
-class Student:
-    def __init__(self, _name, _fam, _otch, _dr, _tel, _id):
-        self.name = _name
-        self.fam = _fam
-        self.otch = _otch
-        self.dr = _dr
-        self.tel = _tel
-        self.id = _id
+class LogIn(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle('Авторизация')
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(QLabel("Логин"))
+        self.login_entry = QLineEdit()
+        layout.addWidget(self.login_entry)
+        layout.addWidget(QLabel("Пароль"))
+        self.pass_entry = QLineEdit()
+        layout.addWidget(self.pass_entry)
+        self.login_btn = QPushButton("ВХОД")
+        layout.addWidget(self.login_btn)
+        self.reg_button = QPushButton("регистрация")
+        layout.addWidget(self.reg_button)
+        self.show()
 
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.students = []
-        self.setGeometry(100, 100, 500, 600)
-        self.setWindowTitle('Студенческий отдел кадров')
-        apply_stylesheet(app, theme='light_blue.xml')
+        self.setWindowTitle('Убийца телеги')
         layout = QGridLayout()
         self.setLayout(layout)
-        self.student_view = QListWidget()
-        self.student_view.currentRowChanged.connect(self.list_item_change)
-        self.fam_entry = QLineEdit()
-        self.name_entry = QLineEdit()
-        self.otchestvo_entry = QLineEdit()
-        self.phone_entry = QLineEdit()
-        self.data_rozhdeniya_entry = QLineEdit()
-        self.add_button = QPushButton("Добавить")
-        self.add_button.clicked.connect(self.add_student)
-        self.edit_button = QPushButton("Изменить")
-        self.edit_button.clicked.connect(self.change_student)
+        self.msg_view = QListWidget()
+        self.msg_entry = QLineEdit()
+        send_btn = QPushButton("Отправить")
+        send_btn.clicked.connect(self.send_msg)
 
-        layout.addWidget(self.student_view, 0, 0, 4, 0)
-        layout.addWidget(self.fam_entry, 1, 1)
-        layout.addWidget(self.name_entry, 1, 3)
-        layout.addWidget(self.otchestvo_entry, 2, 1)
-        layout.addWidget(self.data_rozhdeniya_entry, 2, 3)
-        layout.addWidget(self.phone_entry, 3, 1)
-        layout.addWidget(self.add_button, 3, 2, 1, 2)
-        layout.addWidget(self.edit_button, 4, 0, 1, 2)
+        layout.addWidget(self.msg_view, 0, 0, 1, 2)
+        layout.addWidget(self.msg_entry, 1, 0)
+        layout.addWidget(send_btn, 1, 1)
 
-        layout.addWidget(QLabel("Фамилия"), 1, 0)
-        layout.addWidget(QLabel("Имя"), 1, 2)
-        layout.addWidget(QLabel("Группа"), 2, 0)
-        layout.addWidget(QLabel("Дата рождения"), 2, 2)
-        layout.addWidget(QLabel("Телефон"), 3, 0)
-
-        self.cnx = pymysql.connect(
-            host="localhost",
-            user="pk32",
-            password="1234",
-            database="stud_otdel")
-        self.cur = self.cnx.cursor()
-        self.get_all_students()
-        # show the window
+        self.db_connect()
+        self.get_msg()
         self.show()
 
-    def get_all_students(self):
-        sql = "SELECT * FROM students"
-        self.cur.execute(sql)
-        ans = self.cur.fetchall()
-        for item in ans:
-            st = Student(item[1], item[2], item[3], item[4], item[5], item[0])
-            self.students.append(st)
-            self.student_view.addItem(
-                f"{item[1]}, {item[2]}, {item[3]}, {item[4]}")
+    def db_connect(self):
+        try:
+            self.cnx = pymysql.connect(host="192.168.1.61",
+                                       user="pk32",
+                                       password="1234",
+                                       database="pk32msg",
+                                       cursorclass=pymysql.cursors.DictCursor)
+            self.cur = self.cnx.cursor()
+        except pymysql.Error as e:
+            QMessageBox.critical(self,
+                                 "Ошибка БД",
+                                 f"Ошибка:{e}")
+            self.cnx = None
+            self.cur = None
 
-    def add_student(self):
-        """добавление студентов в базу"""
-        name = self.name_entry.text()
-        fam = self.fam_entry.text()
-        otch = self.otchestvo_entry.text()
-        dr = self.data_rozhdeniya_entry.text()
-        phone = self.phone_entry.text()
-        self.student_view.clear()
-        if name and fam and otch and dr and phone:
-            sql = """INSERT INTO 
-            students(imya, familiya, 
-            otchestvo, data_rozhdeniya, telephone)
-             VALUES(%s,%s,%s,%s,%s)"""
-            params = (name, fam, otch, dr, phone)
-            self.cur.execute(sql, params)
+    def send_msg(self):
+        msg = self.msg_entry.text().strip()
+        if msg and self.cnx:
+            sql = """INSERT INTO msg(text) VALUES(%s)"""
+            self.cur.execute(sql, (msg,))
             self.cnx.commit()
-            self.get_all_students()
-        else:
-            QMessageBox.warning(self, "Внимание!", "Поля должны быть заполнены!")
+            self.msg_entry.clear()
 
-    def change_student(self):
-        """метод для изменения информации о студенте"""
-        x = self.student_view.currentRow()
-        id = self.students[x].id
-        #перенести в другой метод
-        self.name_entry.clear()
-        self.name_entry.insert(self.students[x].name)
+    def get_msg(self):
+        if self.cnx:
+            self.msg_view.clear()
+            self.cur.execute("SELECT text FROM msg")
+            self.cnx.commit()
+            ans = self.cur.fetchall()
+            for item in ans:
+                self.msg_view.addItem(item['text'])
+            self.msg_view.scrollToBottom()
+            QTimer.singleShot(500, self.get_msg)
 
-    def list_item_change(self, row):
-        """метод для определения выбранного студента.
-        Вызывается при изменении пункта списка"""
-        print(row)
-        st = self.students[row]
-        self.selected_id = st.id
-        self.name_entry.setText(st.name)
-        self.fam_entry.setText(st.fam)
-        self.otchestvo_entry.setText(st.otch)
-        self.data_rozhdeniya_entry.setText(str(st.dr))
-        self.phone_entry.setText(st.tel)
-
+    def keyPressEvent(self, event):
+        # Check if the Enter key is pressed
+        if event.key() == 16777220:  # Qt.Key_Return
+            self.send_msg()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # create the main window
-    window = MainWindow()
-    # start the event loop
+    window = LogIn()
     sys.exit(app.exec())
